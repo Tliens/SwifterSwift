8ba046e12e42b38d4f4810b5fff0c9aaea9cf999
quinn <maninios@163.com>
2020-04-22 11:00:59 +0800

    完善UITextViewExtensions

--- Sources/Extensions/UIKit/UITextViewExtensions.swift
+++ Sources/Extensions/UIKit/UITextViewExtensions.swift
@@ -12,27 +12,27 @@ import UIKit
 // MARK: - Methods
 public extension UITextView {
 
-    /// SwifterSwift: Clear text.
+    /// 清空内容
     public func clear() {
         text = ""
         attributedText = NSAttributedString(string: "")
     }
 
-    /// SwifterSwift: Scroll to the bottom of text view
+    /// 滚动到底部
     public func scrollToBottom() {
         // swiftlint:disable next legacy_constructor
         let range = NSMakeRange((text as NSString).length - 1, 1)
         scrollRangeToVisible(range)
     }
 
-    /// SwifterSwift: Scroll to the top of text view
+    /// 滚动到顶部
     public func scrollToTop() {
         // swiftlint:disable next legacy_constructor
         let range = NSMakeRange(0, 1)
         scrollRangeToVisible(range)
     }
 
-    /// SwifterSwift: Wrap to the content (Text / Attributed Text).
+    /// 自动换行到内容(文本/带属性的文本)。
     public func wrapToContent() {
         contentInset = UIEdgeInsets.zero
         scrollIndicatorInsets = UIEdgeInsets.zero
